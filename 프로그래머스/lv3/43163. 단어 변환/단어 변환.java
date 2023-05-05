708c063deaac28c9fa673d2fa65a997cdb933e09
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-05-05 23:24:38 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -3,35 +3,38 @@ class Solution {
     static int answer = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
-        for(int i = 0; i < words.length; i++) {
-            if(compare(begin, words[i]) <= 1) {
+        
+        for(int i = 0; i < words.length; i++){
+            if(compare(begin, words[i]) <= 1){
                 isVisited = new boolean[words.length];
                 isVisited[i] = true;
-                dfs(1, i, target, words);
+                dfs(1, i, words[i], target, words);
             }
         }
         
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
     
-    static void dfs(int cnt, int cur, String target, String[] words) {
-        if(target.equals(words[cur])) {
+    public static void dfs(int cnt, int cur, String begin, String target, String[] words) {
+        
+        if(target.equals(begin)) {
             answer = Math.min(cnt, answer);
             return;
         }
         
-        for (int i = 0; i < words.length; i++) {
-            if(!isVisited[i] && compare(words[cur], words[i]) == 1) {
+        for(int i = 0; i < words.length; i++) {
+            if(!isVisited[i] && compare(begin, words[i]) == 1) {
                 isVisited[i] = true;
-                dfs(cnt + 1, i, target, words);
+                dfs(cnt+1, i, words[i], target, words);
                 isVisited[i] = false;
             }
         }
     }
     
-    static int compare(String s1, String s2) {
+    public static int compare(String s1, String s2) {
         
         int n = 0;
+        
         for(int i = 0; i < s1.length(); i++) {
             if(s1.charAt(i) != s2.charAt(i))
                 n++;
