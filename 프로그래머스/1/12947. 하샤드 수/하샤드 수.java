faec667cdc55bcdd2e4b803ee796972ff3834fd0
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2023-11-08 21:27:26 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean solution(int x) {
+        
+        return hashard(x);
+    }
+    
+    boolean hashard(int x) {
+        int sum = 0, tmp = x;
+        
+        while(tmp > 0) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        
+        return x % sum == 0;
+    }
+}
