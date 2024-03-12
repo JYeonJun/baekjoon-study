831b1f95b3e73fe73b8a976587caec69006d5a1b
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-03-12 16:15:40 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -4,19 +4,9 @@ def solution(left, right):
     answer = 0
     
     for i in range(left, right + 1):
-        if is_divisor_count_even(i): # 짝수라면
-            answer += i
-        else:
+        if int(math.sqrt(i)) == math.sqrt(i):
             answer -= i
+        else:
+            answer += i
     
     return answer
-
-def is_divisor_count_even(num):
-    cnt = 0
-    for i in range(1, int(math.sqrt(num)) + 1):
-        if num % i == 0:
-            if i**2 == num:
-                cnt += 1
-            else:
-                cnt += 2
-    return cnt & 1 == 0
