4834603cfa11ff45f1b81c6f2e901f4e56513664
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-03-29 12:26:41 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 300 ms, Memory: 27200 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -7,26 +7,35 @@ import java.util.Map;
 public class Main {
 
     private static final String ENTER = "ENTER";
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int N;
+    private static final Map<String, Integer> enterCount = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), result = 0;
-        Map<String, Integer> enterCount = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            String name = br.readLine();
-            if (ENTER.equals(name)) {
-                enterCount.clear();
-            } else {
-                enterCount.put(name, enterCount.getOrDefault(name, 0) + 1);
-            }
-
-            if (enterCount.getOrDefault(name, 0) == 1) {
-                result++;
-            }
-        }
+        N = Integer.parseInt(br.readLine());
+        int result = processNames();
 
         System.out.println(result);
+    }
+
+    private static int processNames() throws IOException {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            String name = br.readLine();
+            result += updateEnterCount(name);
+        }
+        return result;
+    }
+
+    private static int updateEnterCount(String name) {
+        if (ENTER.equals(name)) {
+            enterCount.clear();
+            return 0;
+        }
+        if (!enterCount.containsKey(name)) {
+            enterCount.put(name, 1);
+            return 1;
+        }
+        return 0;
     }
 }
