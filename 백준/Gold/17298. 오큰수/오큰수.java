bf0584806955fdd705c90de48587ea15f2ad7020
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-03 17:01:08 +0900

    [Gold IV] Title: 오큰수, Time: 980 ms, Memory: 156068 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -7,53 +9,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        Stack<Node> stack = new Stack<>();
-
         int N = Integer.parseInt(br.readLine());
-
-        int[] resultArr = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        int[] nums = new int[N]; // 원소를 저장할 배열
+        int[] ans = new int[N]; // 오큰수를 저장할 배열
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
 
         for (int i = 0; i < N; i++) {
-
-            int now = Integer.parseInt(st.nextToken());
-
-            while (!stack.isEmpty()) {
-                if (stack.peek().valueNum < now) {
-                    Node node = stack.pop();
-                    resultArr[node.index] = now;
-                } else {
-                    break;
-                }
+            // 스택이 비어있지 않고, 현재 수열의 수가 스택의 top에 있는 인덱스의 수보다 클 때
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                // 오큰수를 ans 배열에 저장
+                ans[stack.pop()] = nums[i];
             }
-
-            stack.push(new Node(i, now));
+            // 인덱스를 스택에 저장
+            stack.push(i);
         }
 
+        // 스택에 남아 있는 인덱스에 대해 오큰수가 없으므로 -1을 저장
         while (!stack.isEmpty()) {
-            Node node = stack.pop();
-            resultArr[node.index] = -1;
+            ans[stack.pop()] = -1;
         }
 
-        for (int i : resultArr) {
-            bw.write(i + " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(ans[i]).append(' ');
         }
 
-        bw.flush();
-        bw.close();
-    }
-
-    static class Node {
-
-        public int index;
-        public int valueNum;
-
-        public Node(int index, int valueNum) {
-            this.index = index;
-            this.valueNum = valueNum;
-        }
+        System.out.println(sb.toString().trim());
     }
 }
