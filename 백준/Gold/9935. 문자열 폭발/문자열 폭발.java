1836f825a628858e92cab7b73bfcb474fe7ca341
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-03 19:53:47 +0900

    [Gold IV] Title: 문자열 폭발, Time: 368 ms, Memory: 37240 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -9,18 +9,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         String bomb = br.readLine();
+
+        int bombLen = bomb.length();
+
         StringBuilder sb = new StringBuilder();
-        
         for (int i = 0; i < input.length(); i++) {
-            sb.append(input.charAt(i)); // 현재 문자를 StringBuilder에 추가합니다.
-            
-            // 폭발 문자열의 마지막 문자와 동일하고, 현재 sb의 길이가 폭발 문자열의 길이보다 크거나 같은 경우,
-            // 폭발 문자열이 sb에 포함되어 있는지 확인합니다.
-            if (sb.length() >= bomb.length() && input.charAt(i) == bomb.charAt(bomb.length() - 1)) {
-                // sb의 끝에서 폭발 문자열의 길이만큼 잘라내 폭발 문자열과 동일한지 확인합니다.
-                if (sb.substring(sb.length() - bomb.length()).equals(bomb)) {
-                    // 폭발 문자열과 동일하다면, 폭발 문자열을 sb에서 제거합니다.
-                    sb.delete(sb.length() - bomb.length(), sb.length());
+            char ch = input.charAt(i);
+            sb.append(ch);
+
+            int sbLen = sb.length();
+            if (sbLen >= bombLen && ch == bomb.charAt(bombLen - 1)) {
+                int startIndex = sbLen - bombLen;
+                if (sb.substring(startIndex).equals(bomb)) {
+                    sb.delete(startIndex, i + sbLen);
                 }
             }
         }
@@ -28,7 +29,7 @@ public class Main {
         if (sb.length() == 0) {
             System.out.println("FRULA");
         } else {
-            System.out.println(sb.toString());
+            System.out.println(sb);
         }
     }
 }
