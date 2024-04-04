4d1afc83d0a1ef375bfd34c02ef207f65d90db96
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-04 11:35:34 +0900

    [Silver I] Title: 절댓값 힙, Time: 328 ms, Memory: 25560 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,40 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
+/*
+-----------------------------------------
+1. 문제의 Input
+- N: 연산의 개수 (1 <= N <= 100,000)
+- x: 연산에 대한 정보 (-2^31 < x < 2^31) - int형 가능
+-> x != 0: 배열에 x 삽입
+-> x == 0: 배열에서 절댓값이 가장 작은 값 출력
+
+2. 요구사항: 0이 주어진 횟수만큼 답 출력
+
+3. 알고리즘
+- PriorityQueue<Integer> 사용
+-> 정렬조건: 절댓값이 같다면 숫자 오름차순, 다르다면 절댓값 오름차순
+
+4. 제한시간: 2초
+
+5. 엣지케이스
+- 배열이 비어있는 경우 0 출력!!
+-----------------------------------------
+ */
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Long> myQueue = new PriorityQueue<>((o1, o2) -> {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> {
+            int abs1 = Math.abs(n1);
+            int abs2 = Math.abs(n2);
 
-            // 절댓값 작은 데이터 우선
-            // 절댓값이 같은 경우 음수 우선
-            long first_abs = Math.abs(o1);
-            long second_abs = Math.abs(o2);
-
-            if (first_abs == second_abs) {
-                return o1 > o2 ? 1 : -1;
+            if (abs1 == abs2) {
+                return n1 - n2;
             }
-
-            return first_abs > second_abs ? 1 : -1;
+            return abs1 - abs2;
         });
 
+        int x;
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            long inputNum = Long.parseLong(br.readLine());
-
-            if (inputNum == 0) {
-                if (myQueue.isEmpty()) {
-                    System.out.println(0);
-                } else {
-                    System.out.println(myQueue.poll());
-                }
+            x = Integer.parseInt(br.readLine());
+            if (x != 0) {
+                pq.add(x);
             } else {
-                myQueue.add(inputNum);
+                if (pq.isEmpty()) {
+                    result.append("0").append("\n");
+                    continue;
+                }
+                result.append(pq.poll()).append("\n");
             }
         }
+
+        System.out.println(result);
     }
 }
