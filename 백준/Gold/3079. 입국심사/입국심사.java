c1715f469869f77c5d6c10fcfddb7d6b79aaa5f0
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-05 15:27:00 +0900

    [Gold V] Title: 입국심사, Time: 340 ms, Memory: 23784 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -1,39 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+        long[] times = new long[(int) N];
 
-        long[] timeTable = new long[n];
-        for (int i = 0; i < n; i++) {
-            timeTable[i] = Integer.parseInt(br.readLine());
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            times[i] = Long.parseLong(br.readLine());
+            min = Math.min(min, times[i]);
         }
 
-        Arrays.sort(timeTable);
-        long start = 1;
-        long end = timeTable[0] * m;
-        long answer = end;
-
+        long start = min, end = min * M, mid = 0, sum = 0, result = Long.MAX_VALUE;
         while (start <= end) {
-           long mid = (start + end)/2;
-           long cnt = 0;
-            for (long time : timeTable) {
-                cnt += (mid / time);
+            mid = (start + end) / 2;
+
+            sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += mid / times[i];
             }
 
-            if (cnt >= m) {
-                end = mid - 1;
-                answer = Math.min(answer, mid);
-            } else {
+            if (sum < M) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        System.out.println(answer);
+
+        System.out.println(start);
     }
 }
