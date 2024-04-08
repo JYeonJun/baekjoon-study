5617f91e47d5a25af0cb3415f066f765f0667284
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-08 15:37:17 +0900

    [Gold V] Title: 적록색약, Time: 164 ms, Memory: 16760 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,74 +5,68 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    private static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
+    private static final int[] dy = {-1, 0, 1, 0};
+    private static final int[] dx = {0, 1, 0, -1};
     private static int N;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         char[][] map1 = new char[N][N];
         char[][] map2 = new char[N][N];
-        boolean[][] visited1 = new boolean[N][N];
-        boolean[][] visited2 = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
             char[] row = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-                char color = row[j];
-                map1[i][j] = color;
-                map2[i][j] = color;
-                if (color == 'G') {
-                    map2[i][j] = 'R';
+                map1[i][j] = row[j];
+                // 'G'를 'R'로 변환하여 map2에 저장
+                map2[i][j] = (row[j] == 'G') ? 'R' : row[j];
+            }
+        }
+
+        System.out.println(getRegionCount(map1) + " " + getRegionCount(map2));
+    }
+
+    private static int getRegionCount(char[][] map) {
+        boolean[][] visited = new boolean[N][N];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    bfs(map, visited, i, j);
+                    count++;
                 }
             }
         }
 
-        int count1 = 0, count2 = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited1[i][j]) {
-                    bfs(map1, visited1, i, j);
-                    count1++;
-                }
-                if (!visited2[i][j]) {
-                    bfs(map2, visited2, i, j);
-                    count2++;
-                }
-            }
-        }
-        System.out.println(count1 + " " + count2);
+        return count;
     }
 
     private static void bfs(char[][] map, boolean[][] visited, int y, int x) {
-        Queue<Pos> que = new LinkedList<>();
-        que.add(new Pos(y, x));
+        Queue<Pos> queue = new LinkedList<>();
+        queue.offer(new Pos(y, x));
         visited[y][x] = true;
 
-        while (!que.isEmpty()) {
-            Pos current = que.poll();
-            int cy = current.y;
-            int cx = current.x;
+        while (!queue.isEmpty()) {
+            Pos current = queue.poll();
 
-            for (int i = 0; i < dy.length; i++) {
-                int ny = cy + dy[i];
-                int nx = cx + dx[i];
+            for (int i = 0; i < 4; i++) {
+                int ny = current.y + dy[i];
+                int nx = current.x + dx[i];
 
-                if (ny < 0 || ny >= N || nx < 0 || nx >= N || visited[ny][nx] || map[cy][cx] != map[ny][nx]) {
-                    continue;
+                if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx] && map[current.y][current.x] == map[ny][nx]) {
+                    queue.offer(new Pos(ny, nx));
+                    visited[ny][nx] = true;
                 }
-
-                que.add(new Pos(ny, nx));
-                visited[ny][nx] = true;
             }
         }
     }
 
     private static class Pos {
-        private int y, x;
+        int y, x;
 
-        public Pos(int y, int x) {
+        Pos(int y, int x) {
             this.y = y;
             this.x = x;
         }
