f6f2cb33b0f4112f566a4dcd0216f8c7de849304
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-04-12 23:53:07 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 248 ms, Memory: 19636 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,100 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int N, result;
-    private static String[][] map;
-    private static boolean[][] visited;
-    private static int[] dx = new int[]{0, 1, 0, -1};
-    private static int[] dy = new int[]{-1, 0, 1, 0};
+    private static final int INF = Integer.MAX_VALUE;
+    private static final int[] DY = {-1, 1, 0, 0}, DX = {0, 0, -1, 1}; // 상, 하, 좌, 우
+
+    private static int N;
+    private static int[][] rupy;
+    private static int[][] matrix;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int cnt = 1;
-        while (true) {
-            N = Integer.parseInt(br.readLine());
+        int count = 1;
+        while ((N = Integer.parseInt(br.readLine())) != 0) {
+            initRupyMatrix(br);
+            initDPMatrix();
 
-            if (N == 0) {
-                return;
-            }
-
-            map = new String[N][N];
-            visited = new boolean[N][N];
-            result = Integer.MAX_VALUE;
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = st.nextToken();
-                }
-            }
-
-            dijkstra(new Pos(0, 0, Integer.parseInt(map[0][0])));
-
-            System.out.println("Problem " + cnt++ + ": " + result);
+            dijkstra(0, 0);
+            System.out.println("Problem " + count++ + ": " + matrix[N - 1][N - 1]);
         }
     }
 
-    private static void dijkstra(Pos pos) {
-        PriorityQueue<Pos> pq = new PriorityQueue<>();
-        visited[pos.y][pos.x] = true;
-        pq.add(pos);
+    private static void dijkstra(int y, int x) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        boolean[][] visited = new boolean[N][N];
+        pq.add(new Node(y, x, rupy[y][x]));
+        visited[y][x] = true;
 
-        int ny, nx, nextY, nextX;
-        Integer nRupee;
         while (!pq.isEmpty()) {
-            Pos nPos = pq.poll();
-            ny = nPos.y;
-            nx = nPos.x;
-            nRupee = nPos.rupee;
+            Node current = pq.poll();
+            int cy = current.y;
+            int cx = current.x;
+            int cw = current.weight;
 
-            if (ny == N - 1 && nx == N - 1) {
-                compare(nRupee);
-                return;
-            }
+            for (int i = 0; i < DY.length; i++) {
+                int ny = cy + DY[i];
+                int nx = cx + DX[i];
 
-            for (int i = 0; i < dx.length; i++) {
-                nextY = ny + dy[i];
-                nextX = nx + dx[i];
-
-                if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= N || visited[nextY][nextX]
-                        || nRupee + Integer.parseInt(map[nextY][nextX]) >
-                        result) {
+                if (isOutOfMatrix(ny, nx) || visited[ny][nx]) {
                     continue;
                 }
 
-                visited[nextY][nextX] = true;
-                pq.add(new Pos(nextY, nextX, nRupee + Integer.parseInt(map[nextY][nextX])));
+                int tmpWeight = matrix[cy][cx] + rupy[ny][nx];
+                if (matrix[ny][nx] >= tmpWeight) {
+                    matrix[ny][nx] = tmpWeight;
+                    pq.add(new Node(ny, nx, tmpWeight));
+                    visited[ny][nx] = true;
+                }
             }
         }
     }
 
-    private static void compare(int num) {
-        result = Math.min(result, num);
-    }
+    private static void initDPMatrix() {
+        matrix = new int[N][N];
 
-    private static class Pos implements Comparable<Pos>{
-        private int y, x;
-        private Integer rupee;
-
-        public Pos(int y, int x, Integer rupee) {
-            this.y = y;
-            this.x = x;
-            this.rupee = rupee;
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(matrix[i], INF);
         }
 
+        matrix[0][0] = rupy[0][0];
+    }
+
+    private static void initRupyMatrix(BufferedReader br) throws IOException {
+        StringTokenizer st;
+        rupy = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                rupy[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private static boolean isOutOfMatrix(int ny, int nx) {
+        return ny < 0 || ny >= N || nx < 0 || nx >= N;
+    }
+
+    private static class Node implements Comparable<Node> {
+        private int y, x, weight;
+
+        public Node(int y, int x, int weight) {
+            this.y = y;
+            this.x = x;
+            this.weight = weight;
+        }
+
+        public void changeWeight(int weight) {
+            this.weight = weight;
+        }
 
         @Override
-        public int compareTo(Pos o) {
-            return this.rupee - o.rupee;
+        public int compareTo(Node node) {
+            return this.weight - node.weight;
         }
     }
 }
