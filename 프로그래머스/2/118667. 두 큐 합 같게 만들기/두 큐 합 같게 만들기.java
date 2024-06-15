e1d7c24038dc4c5078aa825467b7939965697f14
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-06-15 22:31:00 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.20 ms, Memory: 99.5 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -3,52 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
         int answer = 0;
-        Queue<Integer> que1 = new LinkedList<>();
-        Queue<Integer> que2 = new LinkedList<>();
+        Queue<Integer> que1 = new ArrayDeque<>();
+        Queue<Integer> que2 = new ArrayDeque<>();
         
-        for(int i = 0; i < queue1.length; i++) {
-            que1.add(queue1[i]);
+        long que1Sum = 0;
+        long que2Sum = 0;
+
+        for (int num : queue1) {
+            que1.add(num);
+            que1Sum += num;
         }
         
-        for(int i = 0; i < queue2.length; i++) {
-            que2.add(queue2[i]);
+        for (int num : queue2) {
+            que2.add(num);
+            que2Sum += num;
         }
         
-        long que1Sum = que1.stream().mapToInt(Integer::intValue).sum();
-        long que2Sum = que2.stream().mapToInt(Integer::intValue).sum();
         long totalSum = que1Sum + que2Sum;
         
-        if((totalSum & 1) == 1) return -1;
+        if ((totalSum & 1) == 1) return -1;
         
         long targetSum = totalSum / 2;
-        int totalCount = 600_001;
+        int maxOperations = 2 * (queue1.length + queue2.length);
         
-        boolean isEqual = false;
-        while(--totalCount >= 0) {
-            if(que1Sum == targetSum) {
-                isEqual = true;
-                break;
+        while (answer <= maxOperations) {
+            if (que1Sum == targetSum) {
+                return answer;
             }
             
-            if(que1Sum < targetSum) { // 목표합보다 작은 경우
-                // que2에서 빼와서 더하기
+            if (que1Sum < targetSum) {
                 int num = que2.poll();
                 que1.add(num);
                 que1Sum += num;
-            } else { // 목표합보다 큰 경우
-                // que1에서 빼서 que2에 추가
+                que2Sum -= num;
+            } else {
                 int num = que1.poll();
                 que2.add(num);
                 que1Sum -= num;
+                que2Sum += num;
             }
             
             answer++;
         }
         
-        if(!isEqual) {
-            return -1;
-        } else {
-            return answer;
-        }
+        return -1;
     }
 }
