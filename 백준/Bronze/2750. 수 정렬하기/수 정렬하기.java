a1444870b309a047467622a66e2cdfdc3e0583b1
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2024-06-17 18:26:25 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 15196 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,37 +1,21 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        Set<Integer> nums = new TreeSet<>();
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            nums.add(Integer.parseInt(br.readLine()));
         }
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - i - 1; j++) {
-
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = tmp;
-                }
-            }
+        for (Integer num : nums) {
+            System.out.println(num);
         }
-
-        StringBuilder sb = new StringBuilder();
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for (int i : arr) {
-            sb.append(i + "\n");
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
     }
 }
